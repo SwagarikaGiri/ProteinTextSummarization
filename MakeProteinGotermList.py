9daf4cd8af5b928b4c0ba46de2f352f3a0a2e1c6
SwagarikaGiri <swagarika95@gmail.com>
2022-02-23 16:50:53 -0500

    manually added some description files

--- MakeProteinGotermList.py
+++ MakeProteinGotermList.py
@@ -63,6 +63,19 @@ def prepare_csv_file(protein,gos,defination):
             spamwriter.writerow([protein[i],gos[i], defination[i]])
 
             
+def importcsvAndStoreDf():
+    df = pd.read_csv("ProteinDescription.csv", sep = '^')
+    df = df.set_index('Protein')
+    print(df)
+    return df
+
+    # with open('ProteinDescription.txt') as f:
+    #     lines = f.readlines()
+    #     # print(lines)
+    #     for line in lines:
+    #         line =line.strip()
+    #         line_split = line.split("^")
+    #         print(line_split)
 
 def prepareAllGotermAnnotation():
     geneOntology = get_gene_ontology()
@@ -88,8 +101,11 @@ def prepareAllGotermAnnotation():
             'defination':protein_defination })
     print(df)
     df.to_pickle('ProteinDefination.pkl')
-    input()
-    # print(len(goterm_list))
+    return df
+    # input()
+    # # print(len(goterm_list))
 
 
-prepareAllGotermAnnotation()
+# proteinDF= prepareAllGotermAnnotation()
+
+descriptionDF=importcsvAndStoreDf()
