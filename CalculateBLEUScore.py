c44beddde24a6ff394319b3e582c335f0e791d49
SwagarikaGiri <swagarika95@gmail.com>
2022-03-12 23:34:50 -0500

    added stemming and removal of stopword in the code

--- CalculateBLEUScore.py
+++ CalculateBLEUScore.py
@@ -3,11 +3,35 @@ from nltk.translate.bleu_score import sentence_bleu
 import string
 import pandas as pd
 
+#natural language processing packages
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+sw_nltk = stopwords.words('english')
+from nltk.stem import PorterStemmer
+from nltk.stem import LancasterStemmer
+porter = PorterStemmer()
+lancaster=LancasterStemmer()
+from nltk.stem import SnowballStemmer
+snowball = SnowballStemmer(language='english')
+
+
+#importing pickle files
 goaDF = pd.read_pickle('ProteinDefination.pkl')
 goaDF = goaDF.set_index('proteins')
-print(goaDF)
-print(goaDF.loc['P31946','defination'])
 
+
+
+def cleanAndSplitStemming(a_string):
+    a_string=a_string.strip()
+    new_string = a_string.translate(str.maketrans('', '', string.punctuation))
+    # words = [word for word in new_string.split() if word.lower() not in sw_nltk]
+    # print(words)
+    words = [snowball.stem(word) for word in new_string.split() if word.lower() not in sw_nltk]
+    # print(words)
+    # input()
+    # new_text = " ".join(words)
+    return words
 
 def cleanAndSplitSentence(a_string):
     a_string=a_string.strip()
@@ -15,6 +39,8 @@ def cleanAndSplitSentence(a_string):
     return new_string.split()
 
 
+def intersection(lst1, lst2):
+    return list(set(lst1) & set(lst2))
 
 
 def printBlueScores(reference,candidate):
@@ -25,36 +51,34 @@ def printBlueScores(reference,candidate):
     print('Individual 1-2-gram equal weight: %f' % sentence_bleu(reference, candidate, weights=(0.5, 0.5, 0, 0)))
 
 
-
-reference1 = [
-    'this is a dog'.split()
-]
-
-
-candidate1 = 'it is dog'.split()
-# printBlueScores(reference1,candidate1)
-
 reference_df = importcsvAndStoreDf()
-# candidate_df = prepareAllGotermAnnotation()
-def intersection(lst1, lst2):
-    return list(set(lst1) & set(lst2))
 
 for row in reference_df.iterrows():
     protein = row[0]
     print(protein)
     description = row[1][0]
-    # print(str(description))
+    refDescriptionStem = cleanAndSplitStemming(str(description))
+    predDescriptionStem=cleanAndSplitStemming(goaDF.loc[str(row[0]),'defination'])
     refDescription = cleanAndSplitSentence(str(description))
-    # print(refDescription)
-    # print(goaDF.loc[str(row[0]),'defination'])
     predDescription=cleanAndSplitSentence(goaDF.loc[str(row[0]),'defination'])
-    # print(predDescription)
-    print("length of refDescription \t"+str(len(refDescription)))
-    print("length of predDescription \t"+str(len(predDescription)))
-    print("length of intersection \t"+str(len(intersection(refDescription,predDescription))))
+    # print("length of refDescription \t"+str(len(refDescription)))
+    # print("length of predDescription \t"+str(len(predDescription)))
+    # print("length of intersection \t"+str(len(intersection(refDescription,predDescription))))
+    # print("length of refDescriptionStem \t"+str(len(refDescriptionStem)))
+    # print("length of predDescriptionStem \t"+str(len(predDescriptionStem)))
+    # print("length of intersectionStem \t"+str(len(intersection(refDescriptionStem,predDescriptionStem))))
+    print("Intersection of normal sentence just removed the puntuation")
     print(intersection(refDescription,predDescription))
+    print("Intersection of  sentence stemming and stopwords removed")
+    print(intersection(refDescriptionStem,predDescriptionStem))
+    print("Blue of sentence uniprot ref, goa pred")
     printBlueScores(refDescription,predDescription)
+    print("Blue of sentence goa ref, uniprot pred ")
     printBlueScores(predDescription,refDescription)
+    print("Blue of  Stemmed and removed stopwords sentence uniprot ref, goa pred")
+    printBlueScores(refDescriptionStem,predDescriptionStem)
+    print("Blue of Stemmed and removed stopwords sentence goa ref, uniprot pred ")
+    printBlueScores(predDescriptionStem,refDescriptionStem)
     
 
 
